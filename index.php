1242415891d47d7b80e2d26ed581dea13be296fd
Elsie Puig <epuigsant@gmail.com>
2016-09-24 16:04:35 -0700

    add content to index.php file

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Elsie Puig Blank Template</title>
+</head>
+<body>
+  <h1>Blank Wordpress Template</h1>
+  <p>Your content and text goes here.</p>
+</body>
+</html>
