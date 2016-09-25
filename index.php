050b1cd83c1e10e1432fa822a7a3e57983163790
Elsie Puig <epuigsant@gmail.com>
2016-09-24 19:05:08 -0700

    modified styles and index.php page

--- index.php
+++ index.php
@@ -3,9 +3,24 @@
 <head>
   <meta charset="utf-8">
   <title>Elsie Puig Blank Template</title>
+  <!--Link to stylesheet file-->
+  <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>"
 </head>
 <body>
-  <h1>Blank Wordpress Template</h1>
-  <p>Your content and text goes here.</p>
+  <header>
+    <h1>Blank Wordpress Template</h1>
+    <p>Your content and text goes here.</p>
+  </header>
+
+  <section>
+    <h2>This Section is About This</h2>
+    <p>And we'll start out talking about this.</p>
+  </section>
+
+  <footer>
+    <h2>This will conclude the page.</h2>
+    <p>Made by me.</p>
+  </footer>
+
 </body>
 </html>
