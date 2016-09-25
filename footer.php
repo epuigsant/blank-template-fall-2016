bf464bdb218717b980f478d26059fe8beb85b4b8
Elsie Puig <epuigsant@gmail.com>
2016-09-25 12:38:27 -0700

    create widgets and add to footer and sidebar

--- footer.php
+++ footer.php
@@ -1,8 +1,16 @@
     <footer class="row">
-      <div class="twelve column">
-        <h3>This will conclude the page.</h3>
-        <p>Made by me.</p>
+      <div class="four columns">
+        <?php dynamic_sidebar('footer-one'); ?>
       </div>
+
+      <div class="four columns">
+        <?php dynamic_sidebar('footer-two'); ?>
+      </div>
+
+      <div class="four columns">
+        <?php dynamic_sidebar('footer-three'); ?>
+      </div>
+
     </footer>
   </div> <!-- End Container Div-->
   <?php wp_footer(); ?>
